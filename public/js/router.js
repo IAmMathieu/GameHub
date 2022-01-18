434e889c1c62712da864e0d1ea9f864eb90dc244
AtomicMathO <Math.Oclock-Atome.protonmail.com>
2022-01-18 11:01:53 +0100

    Etape 2
    Ajout des liens de navigation vers fourchette et diceRoller

--- public/js/router.js
+++ public/js/router.js
@@ -1,6 +1,18 @@
 const express = require('express');
-
 const router = express.Router();
+const games = require('./../../games.json');
 
+
+router.get('/', (req, res) => {
+    res.render('index')
+})
+
+router.get('/game/fourchette', (req, res) => {
+    res.render('fourchette')
+})
+
+router.get('/game/diceroller', (req, res) => {
+    res.render('diceRoller')
+})
 
 module.exports = router;
