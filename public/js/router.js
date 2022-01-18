2d11ad7fdf270d628d7f8762ebf346fd783d0baf
AtomicMathO <Math.Oclock-Atome.protonmail.com>
2022-01-18 11:17:16 +0100

    Etape 4
    Ajout de la boucle for pour generer les liens de la nav en lien avec games.json

--- public/js/router.js
+++ public/js/router.js
@@ -2,17 +2,23 @@ const express = require('express');
 const router = express.Router();
 const games = require('./../../games.json');
 
-
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        gamesVue: games,
+    })
 })
 
 router.get('/game/fourchette', (req, res) => {
-    res.render('fourchette')
+    res.render('fourchette', {
+        gamesVue: games,
+
+    })
 })
 
 router.get('/game/diceroller', (req, res) => {
-    res.render('diceRoller')
+    res.render('diceRoller', {
+        gamesVue: games,
+    })
 })
 
 module.exports = router;
