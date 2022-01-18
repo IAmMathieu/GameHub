7d84f6b44c3ca3a901ed8f37c2ed58141df1d92a
AtomicMathO <Math.Oclock-Atome.protonmail.com>
2022-01-18 11:39:55 +0100

    Etape 5
    - Gestion dynamique des routes
    
    - Gestion de la 404

--- public/js/router.js
+++ public/js/router.js
@@ -8,17 +8,42 @@ router.get('/', (req, res) => {
     })
 })
 
-router.get('/game/fourchette', (req, res) => {
-    res.render('fourchette', {
-        gamesVue: games,
+router.get('/game/:nomDuJeu', (req, res) => {
+    const askedGame = req.params.nomDuJeu;
 
-    })
+    // on récupère depuis la page retournée par le navigateur (en parm)
+    // l'id de la route du jeu vers lequel on veut aller.
+    // attention on doit qd même vérifier que celle-ce soit valide sinon on retourne une page 404.
+    for (const game of games) {
+        if (game.name === askedGame) {
+            res.render(game.name, {
+            gamesVue: games,
+            })
+        } else {
+            res.status(404);
+            res.render('404');
+        }
+    }
+
+});
+
+router.get('*', (req, res) => {
+    res.status(404);
+    res.render('404', {
+        gamesVue: games,
+    });
 })
 
-router.get('/game/diceroller', (req, res) => {
-    res.render('diceRoller', {
-        gamesVue: games,
-    })
-})
+// router.get('/game/fourchette', (req, res) => {
+//     res.render('fourchette', {
+//         gamesVue: games,
+//     })
+// })
+
+// router.get('/game/diceroller', (req, res) => {
+//     res.render('diceRoller', {
+//         gamesVue: games,
+//     })
+// })
 
 module.exports = router;
